870850fffe6c6dd550f0acf4277346eea5f8952c
Sarah Amselem <correodesarahamselem@gmail.com>
2021-10-27 23:42:32 +0200

    pago

--- Actividad 1 DI/codigocarrito.js
+++ Actividad 1 DI/codigocarrito.js
@@ -1,18 +1,18 @@
+//Inicialización de las variables
 
 var nombreArticulo;
 var precioArticulo;
 var unidadesArticulo;
 var anadirCarrito;
 var formaPago;
-
+ 
 var totalArticulos;
 var totalPrecio;
-
-var errorFaltaArticulo;
-var errorFaltaPrecio;
-var errorTipoIncorrecto;
+ 
+var errorArticulo;
+var errorPrecio;
 var precioNumerico = /^\d+$/;
-
+ 
 var condiciones;
 var imprimir;
 var articulos = [];
@@ -20,73 +20,103 @@ var precioTotal = 0;
 var a;
 var b;
 
+var pago;
+var titularTarjeta;
+var numeroTarjeta;
+var cvv;
+var importeEfectivo;
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 window.onload = function (){
 	init();
-
-
-	
-
-	
+ 
+ 
+ 
+ 
+ 
 }
-//Inicialización de las variables
+ 
 function initVariables(){
 	nombreArticulo = document.getElementById('nombre');
 	precioArticulo = document.getElementById('precio');
 	unidadesArticulo = document.getElementById('unidades');
 	anadirCarrito = document.getElementById('anadir');
 	formaPago = document.getElementById('pago');
-	errorFaltaArticulo = document.getElementById('falta_articulo');
-	errorFaltaPrecio = document.getElementById('falta_precio');
+	errorArticulo = document.getElementById('falta_articulo');
+	errorPrecio = document.getElementById('falta_precio');
 	imprimir = document.getElementById('imprimir');
 	totalArticulos = document.getElementById('totalArticulos');
 	totalPrecio = document.getElementById('totalPrecio');	
 	imprimir = document.getElementById('imprimir');	
 	condiciones = document.getElementById('condiciones');	
 	nombreArticulo.focus();
+	
+	pago = document.getElementById('pago');
+	titularTarjeta = document.getElementById('titularTarjeta');
+	numeroTarjeta = document.getElementById('numeroTarjeta');
+	cvv = document.getElementById('cvv');
+	importeEfectivo = document.getElementById('importeEfectivo');
+	capaTarjeta.style.display = "none";
+	capaEfectivo.style.display = "none";
+
 }
-//Inicialización de los eventos
+ 
 function initEventos(){
-	anadirCarrito.addEventListener("click", anadir);
+	anadirCarrito.addEventListener("click", añadir);
 	imprimir.addEventListener("click", imprimirCompra);
 	condiciones.addEventListener("input", aceptarCondiciones);
+	pago.addEventListener("change", cargarPago);
 }
-
-//Función para añadir al carrito
-function anadir (){
-
-
+ 
+ 
+function añadir (){
+ 
+ 
 	if (nombreArticulo.value == ""){
-		errorFaltaArticulo.textContent = "Falta añadir artículo.";
+		errorArticulo.textContent = "Falta añadir artículo.";
 		a = false;
 	} else{
 		a = true;
-
+ 
 	}
-
+ 
 	if (precioArticulo.value == ""){
-		errorFaltaPrecio.textContent = "Falta añadir precio.";
+		errorPrecio.textContent = "Falta añadir precio.";
 		b = false;
-		}else if (isNaN(precioArticulo)== true){
-		errorTipoIncorrecto.textContent = "Tipo de dato incorrecto.";
+		}else if (false /* FALTA COMPROBAR QUE EL DATO SEA UN NÚMERO!typeof precioArticulo.value === 'number'*/ ){
+		errorPrecio.textContent = "Tipo de dato incorrecto.";
 		b = false
 			}else {
 				b = true;
 			} 
-
-
+ 
+ 
 	if (a && b) {
 		articulos.push(nombreArticulo.value);
-		totalArticulos.textContent =  articulos + " ";
-
+		totalArticulos.textContent = articulos + "";
+ 
 		//NO ME SUMA LOS PRECIOS...
 		precioTotal = (precioTotal + precioArticulo.value);
 		totalPrecio.textContent = precioTotal;
 	}
-
-
+ 
+ 
 	nombreArticulo.value = ("");
 	precioArticulo.value = ("");
-	
+ 
+}
+//Función para proceder al pago
+function cargarPago(){
+	if(pago.value == "seleccione"){
+		capaTarjeta.style.display="none";
+		capaEfectivo.style.display="none";
+	}else if(pago.value == "tarjeta") {
+		capaTarjeta.style.display="block";
+		capaEfectivo.style.display="none";
+	}else{
+		capaTarjeta.style.display="none";
+		capaEfectivo.style.display="block";
+	}	
 }
 
 //NO FUNCIONAA
@@ -97,14 +127,14 @@ function aceptarCondiciones(){
 		imprimir.disabled = true;
 		}
 }
-
+ 
 function imprimirCompra(){
-
+ 
 }
-
-
-
-
+ 
+ 
+ 
+ 
 function init(){
 	initVariables();
 	initEventos();
