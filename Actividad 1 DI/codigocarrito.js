aef9711648cd4e30eba26e64669137905f8d0efe
laurenceapuya <laurence.apuya.p@gmail.com>
2021-10-29 21:37:44 +0200

    Refactorizacion de codigo. Se ha arreglado algunos errores y se ha implementado el alert del pago

--- Actividad 1 DI/codigocarrito.js
+++ Actividad 1 DI/codigocarrito.js
@@ -16,10 +16,8 @@ var precioNumerico = /^\d+$/;
 
 var condiciones;
 var imprimir;
-var articulos = [];
+var articulos = "";
 var precioTotal = 0;
-var a;
-var b;
 
 var pago;
 var titularTarjeta;
@@ -45,7 +43,7 @@ function initVariables(){
 	formaPago = document.getElementById('pago');
 	errorArticulo = document.getElementById('falta_articulo');
 	errorPrecio = document.getElementById('falta_precio');
-  	errorUnidades = document.getElementById('falta_unidades');
+  errorUnidades = document.getElementById('falta_unidades');
 	imprimir = document.getElementById('imprimir');
 	totalArticulos = document.getElementById('totalArticulos');
 	totalPrecio = document.getElementById('totalPrecio');
@@ -64,52 +62,76 @@ function initVariables(){
 
 function initEventos(){
 	anadirCarrito.addEventListener("click", añadir);
-	imprimir.addEventListener("click", imprimirCompra);
 	condiciones.addEventListener("click", aceptarCondiciones);
 	pago.addEventListener("change", cargarPago);
+	imprimir.addEventListener("click", imprimirCompra);
 }
 
+//funcion para comprobar si se ha añadido un articulos
+function checkNombre()
+{
+  if (nombreArticulo.value == ""){
+  		errorArticulo.textContent = "Falta añadir artículo.";
+      return false;
+    } else {
+      return true;
+    }
+}
 
-function añadir (){
-	if (nombreArticulo.value == ""){
-		errorArticulo.textContent = "Falta añadir artículo.";
-		a = false;
-	}else{
-		a = true;
-	}
-
-	if (precioArticulo.value == ""){
+//funcion para comprobar si se ha añadido un precio o si es un numero
+function checkPrecio()
+{
+  if (precioArticulo.value == ""){
 		errorPrecio.textContent = "Falta añadir precio.";
-		b = false;
-  	}else if (isNaN(precioArticulo.value)){
+		return false;
+  } else if (isNaN(precioArticulo.value)){
 		errorPrecio.textContent = "Tipo de dato incorrecto.";
-		b = false
-	}else
-		b = true;
-  //comprobar que las unidades del articulo es mayor que 0
+		return false
+	} else
+    return true;
+}
+
+//funcion para comprobar el numero de unidades introducidas
+function checkUnidades()
+{
   if (unidades.value <= 0) {
     errorUnidades.textContent = "Las unidades no pueden ser 0 o menor.";
-    a = false; //ponemos 'a' falso para que no se añada al carrito
+    return false;
+  } else
+    return true;
 }
 
-  //Si el articulo, el precio y las unidades están bien añadimos
-	if (a && b) {
-		articulos.push(nombreArticulo.value);
-		totalArticulos.value = articulos.concat();
+//funcion para limpair los campos de
+function limpiarErrores()
+{
+	errorPrecio.textContent = "";
+	errorArticulo.textContent = "";
+}
 
-
-		precioTotal += (parseFloat(precioArticulo.value) * parseFloat(unidadesArticulo.value));
-		totalPrecio.value = precioTotal;
-
-    //si tuvieramos algun error
-    errorPrecio.textContent = "";
-    errorArticulo.textContent = "";
-	}
-
-
+//limpia los campos despues de añadir articulos
+function limpiarCampos()
+{
 	nombreArticulo.value = ("");
 	precioArticulo.value = ("");
+}
 
+//añadir aticulos en el carrito
+function añadir (){
+  if (checkNombre() && checkPrecio() && checkUnidades())
+  {
+		//poner comas justo depues del primer articulo
+		if (articulos.length > 0)
+			articulos += ", ";
+    articulos += nombreArticulo.value;
+    totalArticulos.value = articulos + "";
+
+    precioTotal += (parseFloat(precioArticulo.value) * parseFloat(unidadesArticulo.value));
+    totalPrecio.value = precioTotal;
+
+    //si tuvieramos algun error, limpiarlo en el proximo intento
+		limpiarErrores();
+  }
+	limpiarCampos();
 }
 //Función para proceder al pago
 function cargarPago(){
@@ -133,30 +155,52 @@ function aceptarCondiciones(){
 		imprimir.disabled = true;
 		}
 }
-//Función para imprimir compra, falta retocar cosas
-function imprimirCompra(){
-  	var num=/^\d{9}$/;
-	var codigoCvv=/^\d{3}$/;
 
-	if(pago.value=="tarjeta"){
-		if(titularTarjeta.value!="" && numeroTarjeta.value!="" && cvv.value!=""){
-			if(!isNan(numeroTarjeta.value) && codigoCvv.test(cvv.value)){
-				alert("procesando datos");
-			}else{
-				alert("datos incorrectos...");
-			}
-		}else if(pago.value=="efectivo"){
-			if(importe.value!="" &&  !isNaN(importe.value)){
-				alert("procesando datos");
-			}else{
-				alert("datos de importe efectivo incorrectos");
-			}
-		}else alert("seleccione un método de pago");
-	}
+//funcion auxiliar que comprueba si un string esta vacio o nulo
+function isEmpty(str)
+{
+	return (!str || str.length == 0 );
 }
 
+//función que servira para imprimir todo despues del pago
+function imprimirTodo()
+{
+	return ("Los articulos son: " + totalArticulos.value +
+	"\nEl precio total es: " + totalPrecio.value +
+	"\nForma de pago es: " + pago.value);
+}
 
+//funcion que se activa al pagar con tarjeta
+function pagoConTarjeta()
+{
+	var codigoCvv=/^\d{3}$/;
 
+	//comprobamos si algun campo esta vacio
+	if(!isEmpty(titularTarjeta.value) && !isEmpty(numeroTarjeta.value) && !isEmpty(cvv.value)){
+		if(!isNaN(numeroTarjeta.value) && codigoCvv.test(cvv.value)) //que sean numeros y 3 numeros de cvv
+			alert(imprimirTodo());
+		else
+			alert("datos incorrectos...");
+	}else
+		alert("faltan datos");
+}
+
+//funcion que se activa al pagar con efectivo
+function pagoConEfectivo()
+{
+	if(importe.value!="" &&  !isNaN(importe.value))
+		alert(imprimirTodo());
+	else
+		alert("datos de importe efectivo incorrectos");
+}
+//Función para imprimir compra, falta retocar cosas
+function imprimirCompra(){
+	if(pago.value=="tarjeta"){
+		pagoConTarjeta();
+	}else if(pago.value=="efectivo"){
+		pagoConEfectivo
+	}else alert("seleccione un método de pago");
+}
 
 function init(){
 	initVariables();
