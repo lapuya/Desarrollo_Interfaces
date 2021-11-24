84eef9037a19961e66dfa1ab21da9e468f2b020d
laurenceapuya <laurence.apuya.p@gmail.com>
2021-11-24 20:22:08 +0100

    Arreglando el MVC

--- Actividad_2/2_Calculadora/src/es/vista/VistaCalculadora.java
+++ Actividad_2/2_Calculadora/src/es/vista/VistaCalculadora.java
@@ -8,8 +8,12 @@ import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
+import es.controlador.ControladorCalculadora;
+
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VistaCalculadora extends JFrame {
 	
@@ -41,6 +45,11 @@ public class VistaCalculadora extends JFrame {
 	private JRadioButton botonRadioTroll;
 	private JPanel panelPantalla;
 	private JLabel pantalla;
+	
+	private String resultado = "0";
+	private String operacion = "";
+	private String ultimoNumero = "";
+	private boolean nuevaOperacion = true;
 
 	/**
 	 * Create the panel.
@@ -57,82 +66,122 @@ public class VistaCalculadora extends JFrame {
 		boton0 = new JButton("0");
 		boton0.setBounds(17, 385, 117, 29);
 		getContentPane().add(boton0);
+		boton0.setActionCommand("ceri");
+
 		
 		boton1 = new JButton("1");
+		boton1.setActionCommand("uno");
 		boton1.setBounds(17, 344, 117, 29);
 		getContentPane().add(boton1);
 		
 		boton2 = new JButton("2");
 		boton2.setBounds(149, 344, 117, 29);
 		getContentPane().add(boton2);
+		boton2.setActionCommand("dos");
+
 		
 		boton3 = new JButton("3");
 		boton3.setBounds(278, 344, 117, 29);
 		getContentPane().add(boton3);
+		boton3.setActionCommand("tres");
+
 		
 		boton4 = new JButton("4");
 		boton4.setBounds(17, 303, 117, 29);
 		getContentPane().add(boton4);
+		boton4.setActionCommand("cuatro");
+
 		
 		boton5 = new JButton("5");
 		boton5.setBounds(149, 303, 117, 29);
 		getContentPane().add(boton5);
+		boton5.setActionCommand("cinco");
+
 		
 		boton6 = new JButton("6");
 		boton6.setBounds(278, 303, 117, 29);
 		getContentPane().add(boton6);
+		boton6.setActionCommand("seis");
+
 		
 		boton7 = new JButton("7");
 		boton7.setBounds(17, 262, 117, 29);
 		getContentPane().add(boton7);
+		boton7.setActionCommand("siete");
+
 		
 		boton8 = new JButton("8");
 		boton8.setBounds(149, 262, 117, 29);
 		getContentPane().add(boton8);
+		boton8.setActionCommand("ocho");
+
 		
 		boton9 = new JButton("9");
 		boton9.setBounds(278, 262, 117, 29);
 		getContentPane().add(boton9);
+		boton9.setActionCommand("nueve");
+
 		
 		botonPunto = new JButton(".");
 		botonPunto.setBounds(525, 385, 117, 29);
 		getContentPane().add(botonPunto);
+		botonPunto.setActionCommand(".");
+
 		
 		botonResultado = new JButton("=");
 		botonResultado.setBounds(525, 262, 117, 29);
 		getContentPane().add(botonResultado);
+		botonResultado.setActionCommand("=");
+
+		
 		
 		botonCubica = new JButton("∛");
 		botonCubica.setBounds(146, 385, 117, 29);
 		getContentPane().add(botonCubica);
+		botonCubica.setActionCommand("∛");
+
 		
 		botonCuadrada = new JButton("√");
 		botonCuadrada.setBounds(278, 385, 117, 29);
 		getContentPane().add(botonCuadrada);
+		botonCuadrada.setActionCommand("√");
+
 		
 		botonSuma = new JButton("+");
 		botonSuma.setBounds(406, 262, 117, 29);
 		getContentPane().add(botonSuma);
+		botonSuma.setActionCommand("+");
+
 		
 		botonResta = new JButton("-");
 		botonResta.setBounds(407, 303, 117, 29);
 		getContentPane().add(botonResta);
+		botonResta.setActionCommand("-");
+
 		
 		botonMultiplicar = new JButton("x");
 		botonMultiplicar.setBounds(407, 344, 117, 29);
 		getContentPane().add(botonMultiplicar);
+		botonMultiplicar.setActionCommand("x");
+
 		
 		botonDividir = new JButton("/");
 		botonDividir.setBounds(406, 385, 117, 29);
 		getContentPane().add(botonDividir);
+		botonDividir.setActionCommand("/");
+
 		
 		botonBorrar = new JButton("DEL");
 		botonBorrar.setBounds(525, 303, 117, 29);
 		getContentPane().add(botonBorrar);
+		botonBorrar.setActionCommand("delete");
+
 		
 		botonLimpiar = new JButton("CLEAN");
 		botonLimpiar.setBounds(525, 344, 117, 29);
 		getContentPane().add(botonLimpiar);
+		botonLimpiar.setActionCommand("clean");
+
 		
 		titulo = new JLabel("Just a Normal Calculator :)");
 		titulo.setBounds(237, 18, 176, 42);
@@ -157,7 +206,7 @@ public class VistaCalculadora extends JFrame {
 		getContentPane().add(panelPantalla);
 		panelPantalla.setLayout(null);
 		
-		pantalla = new JLabel("0");
+		pantalla = new JLabel(resultado);
 		pantalla.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
 		pantalla.setHorizontalAlignment(SwingConstants.RIGHT);
 		pantalla.setBounds(6, 6, 608, 74);
@@ -171,6 +220,66 @@ public class VistaCalculadora extends JFrame {
 		setVisible(true);
 		
 	}
+	
+	public void setControlador(ControladorCalculadora c) {
+		boton0.addActionListener(c);
+		boton1.addActionListener(c);
+		boton2.addActionListener(c);
+		boton3.addActionListener(c);
+		boton4.addActionListener(c);
+		boton5.addActionListener(c);
+		boton6.addActionListener(c);
+		boton7.addActionListener(c);
+		boton8.addActionListener(c);
+		boton9.addActionListener(c);
+		botonPunto.addActionListener(c);
+		botonResultado.addActionListener(c);
+		botonCubica.addActionListener(c);
+		botonCuadrada.addActionListener(c);
+		botonSuma.addActionListener(c);
+		botonResta.addActionListener(c);
+		botonMultiplicar.addActionListener(c);
+		botonDividir.addActionListener(c);
+		botonBorrar.addActionListener(c);
+		botonLimpiar.addActionListener(c);
+		botonResultado.addActionListener(c);
+	}
 
+	public void setPantalla(String value) {
+		// TODO Auto-generated method stub
+		pantalla.setText(value);
+	}
+
+	public void setUltimoNumero(String numero) {
+		ultimoNumero = numero;
+		
+	}
+
+	public String getUltimoNumero() {
+		// TODO Auto-generated method stub
+		return ultimoNumero;
+	}
+
+	public void setOperacion(String op) {
+		// TODO Auto-generated method stub
+		operacion = op;
+		
+	}
+
+	public void setNuevaOperacion(boolean b) {
+		nuevaOperacion =  b;
+		
+	}
+
+	public boolean getNuevaOperacion() {
+		// TODO Auto-generated method stub
+		return nuevaOperacion;
+	}
+
+	public String getOperacion() {
+		// TODO Auto-generated method stub
+		return operacion;
+	}
 
 }
+
